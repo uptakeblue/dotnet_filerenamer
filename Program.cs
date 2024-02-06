deb5827739f13e934811cff70664892d560a44df
Michael Rubin <github@uptakeblue.com>
2024-02-06 10:10:37 -0800

    initial upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotnet_filerenamer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine(2 + 2);
+    }
+  }
+}
